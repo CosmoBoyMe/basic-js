4a5e4367585706fbebfd3ed7ca36fc1cb5f6f242
CosmoboyMe <2014failz2014@gmail.com>
2022-10-16 22:40:09 +0200

    create getDNSStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+
+  domains.forEach((item) => {
+    const splittedItem = item.split(".").reverse();
+    let temp = "";
+    splittedItem.forEach((item) => {
+      const currentDns = `${temp}.${item}`;
+      if (result[currentDns] === undefined) {
+        result[currentDns] = 1;
+        temp = currentDns;
+      } else {
+        result[currentDns] += 1;
+        temp = currentDns;
+      }
+    });
+  });
+
+  return result;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
